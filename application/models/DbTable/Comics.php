b86de4b5da31c3ef3241046f8da6061a20630353
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2012-08-25 10:44:08 -0400

    Basic get/add/update/delete functions

--- application/models/DbTable/Comics.php
+++ application/models/DbTable/Comics.php
@@ -4,6 +4,39 @@ class Application_Model_DbTable_Comics extends Zend_Db_Table_Abstract
 {
 
     protected $_name = 'comics';
+	
+	public function getComic($id)
+	{
+		$id = (int)$id;
+		$row = $this->fetchRow('id = ' . $id);
+		if (!$row) {
+			throw new Exception("Could not find row $id");
+		}
+		return $row->toArray();	
+	}
+	
+	public function addComic($issuenr, $title)
+	{
+		$data = array(
+			'issuenr' => $issuenr,
+			'title' => $title,
+		);
+		$this->insert($data);	
+	}
+	
+	public function updateComic($id, $issuenr, $title)
+	{
+		$data = array(
+			'issuenr' => $issuenr,
+			'title' => $title,
+		);
+		$this->update($data, 'id = ' . (int)$id);
+	}
+	
+	public function deleteComic($id)
+	{
+		$this->delete('id =' . (int)$id);
+	}
 
 
 }
