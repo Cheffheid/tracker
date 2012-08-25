5e93254e87ebf5194da988ef72acfae9734b8fa4
Jeffrey de Wit <Jeffrey.deWit@gmail.com>
2012-08-25 14:22:04 -0400

    Added documenting comments

--- application/controllers/MusicController.php
+++ application/controllers/MusicController.php
@@ -25,8 +25,12 @@ class MusicController extends Zend_Controller_Action
 			$sortparams = null;
 		}
 		
+		
 		$this->view->albums = $albums->fetchAll(null, "$sortparams");
 		
+		/* Set view variables for the sortorder link
+			Basically, if a column is sorted ascending, set the variable to descending and vice versa
+		*/
 		if ( $this->_request->getParam('sortorder') === "ASC" ) {
 			if($this->_request->getParam('sort') === "artist") {
 				$this->view->titleorder = "ASC";
@@ -43,10 +47,13 @@ class MusicController extends Zend_Controller_Action
 
     public function addAction()
     {
+		// Create a new form to add stuff
         $form = new Application_Form_Album();
 		$form->submit->setLabel('Add');
 		$this->view->form = $form;
 		
+		// If a form was submitted to this page, validate it, add the new album and go back to the list
+		// Otherwise, just render the form again with the posted data
 		if ($this->getRequest()->isPost()) {
 			$formData = $this->getRequest()->getPost();
 		
@@ -65,6 +72,7 @@ class MusicController extends Zend_Controller_Action
 
     public function editAction()
     {
+		// Create the form for editing an entry
         $form = new Application_Form_Album();
 		$form->submit->setLabel('Save');
 		$this->view->form = $form;
@@ -72,6 +80,8 @@ class MusicController extends Zend_Controller_Action
 		if ($this->getRequest()->isPost()) {
 			$formData = $this->getRequest()->getPost();
 		
+		// If a form was submitted to this page, validate it, edit the album and go back to the list
+		// Otherwise, just render the form again with the posted data
 			if ($form->isValid($formData)) {
 				$id = (int)$form->getValue('id');
 				$artist = $form->getValue('artist');
@@ -94,6 +104,7 @@ class MusicController extends Zend_Controller_Action
 
     public function deleteAction()
     {
+		// Check if "Yes" was clicked at the confirmation, if so, delete the album else just return to the list
         if ($this->getRequest()->isPost()) {
             $del = $this->getRequest()->getPost('del');
             if ($del == 'Yes') { 
--- application/forms/Album.php
+++ application/forms/Album.php
@@ -6,9 +6,12 @@ class Application_Form_Album extends Zend_Form
 	public function init()
 	{
 		$this->setName('album');
+		
+		// Add hidden field "id"
 		$id = new Zend_Form_Element_Hidden('id');
 		$id->addFilter('Int');
 		
+		// Add text field "artist"
 		$artist = new Zend_Form_Element_Text('artist');
 		$artist->setLabel('Artist')
 			   ->setRequired(true)
@@ -16,17 +19,19 @@ class Application_Form_Album extends Zend_Form
 			   ->addFilter('StringTrim')
 			   ->addValidator('NotEmpty');
 			   
+		// Add text field "title"
 		$title = new Zend_Form_Element_Text('title');
 		$title->setLabel('Title')
 			  ->setRequired(true)
 			  ->addFilter('StripTags')
 			  ->addFilter('StringTrim')
 			  ->addValidator('NotEmpty');
-			  
+		
+		// Add submit button
 		$submit = new Zend_Form_Element_Submit('submit');
 		$submit->setAttrib('id', 'submitbutton');
+		
 		$this->addElements(array($id, $artist, $title, $submit));
 	}
-
 }
 
